926512b6e2cc3667721031887b42822697ba26a7
jigmet12 <jigmetashi02@gmail.com>
2020-09-05 23:50:53 +0400

    added login page

--- covid19/src/App.js
+++ covid19/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/login/Login';
+import styles from './app.module.css';
+import Sidebar from './components/sideBar/Sidebar';
+import Home from './components/Home/Home';
 
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className={styles.app}>
+                <Sidebar/>
+                <Switch>
+                    <div className={styles.app__body}>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/login" component={Login}/>
+                    </div>
+                </Switch>
+            </div>
+
+        </Router>
+
+    );
 }
 
 export default App;
