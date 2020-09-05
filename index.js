c9b50fd40117116635eb5f3134c7e818f5ec7768
Bryan H <bhuang453@yahoo.com>
2020-09-05 12:14:08 -0700

    api key

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function initMap() {
     zoom: 8
   });
 }
+
+// Create the script tag, set the appropriate attributes
+var script = document.createElement("script");
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyDyWSJ1bU2zw9y-zIMrYTwB5S83RGWjpx4&callback=initMap";
+script.defer = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function() {
+  // JS API is loaded and available
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
