10ca3db3fbb9c15b8e3834f3919614a85b05c87a
jigmet12 <jigmetashi02@gmail.com>
2020-09-06 16:03:18 +0400

    fixed the map

--- covid19/src/components/map/Map.js
+++ covid19/src/components/map/Map.js
@@ -3,25 +3,30 @@ import styles from './map.module.css';
 import {Map as LeafletMap, TileLayer} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import {Circle, Popup} from 'react-leaflet';
+import {Button} from '@material-ui/core';
+import {Link} from 'react-router-dom';
 
 
 function Map() {
     return (
         <div className={styles.mapL}>
-            <LeafletMap className={styles.leafletContainer} center={[0, 0]} zoom={3} minZoom={2.5}>
+            <LeafletMap className={styles.leafletContainer} center={[43.6532, -79.3832]} zoom={5} minZoom={5} maxZoom={12}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/">OpenStreetMap</a> contributors'/>
                 <Circle
-                    center={[27.5142, 90.4336]}
+                    center={[43.6532, -79.3832]}
                     fillOpacity={0.8}
                     color="blue"
                     fillColor="blue"
                     radius= {109900}>
-                    <Popup>
-                        <div className="info__container">
-                            <div className='info__eventName'>Grocery</div>
-                            <div className="info__name">"Bhutan"</div>
+                    <Popup className={styles.popUp}>
+                        <div className={styles.info__container}>
+                            <div className={styles.info__eventName}>Grocery</div>
+                            <div className={styles.info__place}>"Usa, Toronto "</div>
+                            <Link to="/register"><Button className={styles.volunteerBtn}>
+                                volunteer
+                            </Button></Link>
                         </div>
                     </Popup>
                 </Circle>
